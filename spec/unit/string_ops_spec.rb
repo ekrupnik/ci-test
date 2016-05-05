b6d66d71d89f9db23afca557d0f844956cdbb056
Eric Krupnik <eric.krupnik@perkinelmer.com>
2016-05-05 13:51:19 -0400

    Update

--- spec/unit/string_ops_spec.rb
+++ spec/unit/string_ops_spec.rb
@@ -15,26 +15,26 @@ describe StringOps do
     end
   end
 
-  # describe "#to_lower" do
-  #   it "returns lower case for input_string" do
-  #     expected_string = "to see a world in a grain of sand, and a heaven in a wild flower, hold infinity in the palm of your hand, and eternity in an hour."
-  #     expect(string_ops.to_lower(input_string)).to eq(expected_string)
-  #   end
-  # end
+  describe "#to_lower" do
+    it "returns lower case for input_string" do
+      expected_string = "to see a world in a grain of sand, and a heaven in a wild flower, hold infinity in the palm of your hand, and eternity in an hour."
+      expect(string_ops.to_lower(input_string)).to eq(expected_string)
+    end
+  end
 
-  # describe "#to_empty" do
-  #   it "returns empty string" do
-  #     expected_string = ""
-  #     expect(string_ops.to_empty(input_string)).to eq(expected_string)
-  #   end
-  # end
+  describe "#to_empty" do
+    it "returns empty string" do
+      expected_string = ""
+      expect(string_ops.to_empty(input_string)).to eq(expected_string)
+    end
+  end
 
-  # describe "#reverse" do
-  #   it "returns the reversed string" do
-  #     input_string = "input"
-  #     expected_string = "tupni"
-  #     expect(string_ops.reverse(input_string)).to eq(expected_string)
-  #   end
-  # end
+  describe "#reverse" do
+    it "returns the reversed string" do
+      input_string = "input"
+      expected_string = "tupni"
+      expect(string_ops.reverse(input_string)).to eq(expected_string)
+    end
+  end
 
 end
