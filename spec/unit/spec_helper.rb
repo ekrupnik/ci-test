2e36c2802bcab917cd8925fdf5e406fe02981ec2
Eric Krupnik <eric.krupnik@perkinelmer.com>
2016-05-05 11:57:21 -0400

    Comment test for code coverage demo

--- spec/unit/spec_helper.rb
+++ spec/unit/spec_helper.rb
@@ -14,14 +14,14 @@ describe StringOps do
   describe "#to_upper" do
     it "returns upper case for input_string" do
       expected_string = "TO SEE A WORLD IN A GRAIN OF SAND, AND A HEAVEN IN A WILD FLOWER, HOLD INFINITY IN THE PALM OF YOUR HAND, AND ETERNITY IN AN HOUR."
-      expect(string_ops.to_upper(input_string)).to eq(expected_string)
+#      expect(string_ops.to_upper(input_string)).to eq(expected_string)
     end
   end
 
   describe "#to_lower" do
     it "returns lower case for input_string" do
       expected_string = "to see a world in a grain of sand, and a heaven in a wild flower, hold infinity in the palm of your hand, and eternity in an hour."
-      expect(string_ops.to_lower(input_string)).to eq(expected_string)
+#      expect(string_ops.to_lower(input_string)).to eq(expected_string)
     end
   end
 end
