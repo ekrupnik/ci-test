f522292ce4bfaebfa58dc3a4ff5fe4adda8ce7c8
Eric Krupnik <eric.krupnik@perkinelmer.com>
2016-05-05 13:18:52 -0400

    Demo

--- spec/unit/string_ops_spec.rb
+++ spec/unit/string_ops_spec.rb
@@ -11,22 +11,30 @@ describe StringOps do
   describe "#to_upper" do
     it "returns upper case for input_string" do
       expected_string = "TO SEE A WORLD IN A GRAIN OF SAND, AND A HEAVEN IN A WILD FLOWER, HOLD INFINITY IN THE PALM OF YOUR HAND, AND ETERNITY IN AN HOUR."
-#      expect(string_ops.to_upper(input_string)).to eq(expected_string)
+      expect(string_ops.to_upper(input_string)).to eq(expected_string)
     end
   end
 
-  describe "#to_lower" do
-    it "returns lower case for input_string" do
-      expected_string = "to see a world in a grain of sand, and a heaven in a wild flower, hold infinity in the palm of your hand, and eternity in an hour."
-#      expect(string_ops.to_lower(input_string)).to eq(expected_string)
-    end
-  end
+  # describe "#to_lower" do
+  #   it "returns lower case for input_string" do
+  #     expected_string = "to see a world in a grain of sand, and a heaven in a wild flower, hold infinity in the palm of your hand, and eternity in an hour."
+  #     expect(string_ops.to_lower(input_string)).to eq(expected_string)
+  #   end
+  # end
 
-  describe "#to_empty" do
-    it "returns empty string" do
-      expected_string = ""
-   #   expect(string_ops.to_empty(input_string)).to eq(expected_string)
-    end
-  end
+  # describe "#to_empty" do
+  #   it "returns empty string" do
+  #     expected_string = ""
+  #     expect(string_ops.to_empty(input_string)).to eq(expected_string)
+  #   end
+  # end
+
+  # describe "#reverse" do
+  #   it "returns the reversed string" do
+  #     input_string = "input"
+  #     expected_string = "tupni"
+  #     expect(string_ops.reverse(input_string)).to eq(expected_string)
+  #   end
+  # end
 
 end
